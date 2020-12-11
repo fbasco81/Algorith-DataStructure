8ddfb94b84bc534e5eaebaace7df0fb18c3d6efd
Francesco Basco <francesco.basco@msc.com>
2020-12-11 14:58:07 +0100

    good job

--- w5 - 02 - PrimitiveCalculator/test.program.js
+++ w5 - 02 - PrimitiveCalculator/test.program.js
@@ -15,5 +15,12 @@ describe("Primitive calculator", function () {
         sequence: [1, 2, 4, 5],
       });
     });
+
+    it("should return 14 when the input is 96234", function () {
+      assert.strictEqual(program.calculate(96234), {
+        operations: 14,
+        sequence: [1, 3,9,10,11,22,66,198,594,1782,5346,16038,16039,32078,96234],
+      });
+    });
   });
 });
