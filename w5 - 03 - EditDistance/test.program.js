bdc043f96185388fdd95c24016b9a57cc4d9aa47
Francesco Basco <fbasco81@gmail.com>
2020-12-16 23:13:47 +0100

    good job!

--- w5 - 03 - EditDistance/test.program.js
+++ w5 - 03 - EditDistance/test.program.js
@@ -1,4 +1,5 @@
 var assert = require("assert");
+const { generateWords } = require("./program");
 var program = require("./program");
 describe("Edit distance", function () {
   describe("case (1)", function () {
@@ -12,6 +13,17 @@ describe("Edit distance", function () {
 
     it("should return 5 when the input is editing - distance", function () {
       assert.strictEqual(program.distance("editing", "distance"), 5);
+    });
+
+    it("should return true ", function () {
+      for (var i = 0; i < 100; i++) {
+        var rnd = Math.round(Math.random() * 100);
+        var words = generateWords(2);
+
+        var distance1 = program.distance(words[0], words[1]);
+        var distance2 = program.levenshteinDistance(words[0], words[1]);
+        assert.strictEqual(distance1, distance2, words[0] + " !== " + words[1]);
+      }
     });
   });
 });
